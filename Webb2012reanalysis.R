3beaa88cecfbe3483d9154919e46b2d4347c4d81
Ivan Ropovik <ivan.ropovik@gmail.com>
2020-03-31 12:01:49 +0200

    tyding up, separating the mixed domain

--- Webb2012reanalysis.R
+++ Webb2012reanalysis.R
@@ -1,3 +1,19 @@
+#' ---
+#' title: "Re-analysis of Webb et al. 2012 meta-analysis"
+#' author: "Ivan Ropovik"
+#' output:
+#'    html_document:
+#'       code_folding: hide
+#'       toc: true
+#'       toc_float: true
+#'       fig_retina: 2
+#'       theme: paper
+#' always_allow_html: yes
+#' ---
+#+ setup, include=FALSE
+knitr::opts_chunk$set(echo=FALSE)
+
+# Libraries
 library(tidyr)
 library(metafor)
 library(compute.es)
@@ -12,29 +28,36 @@ corr <- .5
 briefBias <- TRUE
 # Subset only the designs with a control group?
 RCTonly <- 1 # If no, set to 0
+# Subset only the experiential designs?
+Experiential <- 1
 
+# Source funcions
 source("pcurve.R")
 source("functions.R")
 
-# read in the data
+#'#### Re-analysis settings
+paste("Subsetting only the designs with a control group set to ", RCTonly, ".", sep = "")
+paste("Subsetting only experiential designs set to ", Experiential, ".", sep = "")
+
+# Read in the data
 dataWide <- read.csv("Webb 2012 meta re-analysis.csv")
 
-# define factors
+# Define factors
 dataWide$study <- as.factor(dataWide$study)
 levels(dataWide$effect) <- c("reappraise emotional response", "reappraise emotional stimulus",
                          "reappraise via perspective taking", "reappraisal–mixed")
 
-# convert from wide to long and order
+# Convert from wide to long and order
 data <- gather(dataWide, domain, yiD, experiential:physiological, na.rm = T)
 data <- data[order(data$study),]
 
-# compute ES, variances, and p-values for two-group designs
+# Compute ES, variances, and p-values for two-group designs
 es2g <- with(data[!is.na(data$nCtrl),], des(yiD, nExp, nCtrl, id = study, verbose = F, dig = 1e10))
 data$yi[!is.na(data$nCtrl)] <- es2g$g
 data$vi[!is.na(data$nCtrl)] <- es2g$var.g
 data$p[!is.na(data$nCtrl)] <- es2g$pval.g
 
-# compute ES, variances, and p-values for one-group designs
+# Compute ES, variances, and p-values for one-group designs
 corr <- .5 # Assuming pre-post correlation of r = .5
 data$yi[is.na(data$nCtrl)] <- with(data[is.na(data$nCtrl),], (1 - (3/(4*nExp - 3))) * yiD)
 data$vi[is.na(data$nCtrl)] <- with(data[is.na(data$nCtrl),], (1 - (3/(4*nExp - 3)))^2 * ((1 / nExp) + ((yiD^2) / (2 * nExp))) * 2 * (1 - corr))
@@ -43,6 +66,13 @@ data$p[is.na(data$nCtrl)] <- with(data[is.na(data$nCtrl),], 2*pt(abs(yi/sqrt(vi)
 # Create a results label based on p-values, converted to z-score.
 data$zPcurve <- paste(data$study, ": ", "Z=", qnorm(1-(data$p)/2), sep = "")
 
+#'# RMA for mixed reappraisal
+#'
+#' Reproducing the RMA for mixed reappraisal
+#+ include=TRUE
+dataMix <- data[data$effect == "reappraisal–mixed",]
+rmaCustom(dataMix)
+
 # Subset only data for designs with a control group
 if(RCTonly == 1){
   data <- data[!is.na(data$nCtrl),]
@@ -50,23 +80,25 @@ if(RCTonly == 1){
   paste("Result including designs lacking a control group")
 }
 
+# Subset only data for experiential domain
+if(Experiential == 1){
+  data <- data[data$domain == "experiential",]
+} else {
+  paste("Result including physiological and behavioral effects")
+}
+
 # Subset data objects
 dataResp <- data[data$effect == "reappraise emotional response",]
 dataStim <- data[data$effect == "reappraise emotional stimulus",]
 dataPers <- data[data$effect == "reappraise via perspective taking",]
-dataMix <- data[data$effect == "reappraisal–mixed",]
 
 # Meta-analysis -----------------------------------------------------------
-
-#'## Meta-analysis
+#'# Meta-analysis and bias correction
 #'
-#'k = number of studies; sqrt in "Variance components" = tau, the standard deviation of true effects; estimate in "Model results" = naive MA estimate
+#'For reappraise emotional response, reappraise emotional stimulus, and reappraise via perspective taking, respectively
 namesObjects <- levels(data$effect)[-4]
 dataObjects <- list("reappraise emotional response" = dataResp, "reappraise emotional stimulus" = dataStim, "reappraise via perspective taking" = dataPers)
 rmaObjects <- setNames(lapply(dataObjects, function(x){rmaCustom(x)}), nm = namesObjects)
-
-# RMA for mixed reappraisal
-rmaCustom(dataMix)
 
 results <- list(NA)
 for(i in 1:length(rmaObjects)){
@@ -76,4 +108,7 @@ for(i in 1:length(rmaObjects)){
 results <- setNames(results, nm = namesObjects)
 results
 
-rmaObjects <- setNames(lapply(rmaObjects, function(x){metafor::funnel(x, level=c(90, 95, 99), shade=c("white", "gray", "darkgray"), refline=0, pch = 20, yaxis = "sei")}), nm = namesObjects)
+#'## Funnel plots
+#'
+#'Funnel plots for reappraise emotional response, reappraise emotional stimulus, and reappraise via perspective taking, respectively
+rmaObjects <- lapply(setNames(rmaObjects, nm = namesObjects), function(x){metafor::funnel(x, level=c(90, 95, 99), shade=c("white", "gray", "darkgray"), refline=0, pch = 20, yaxis = "sei")})
